06319a2fb758dca5bd6a7c61aaaa4256ca056da1
Elad Harizy <elad.harizy@gmail.com>
2020-03-05 21:25:16 +0200

    Started C# project, with words hello world and yes

---
+++ fluffy-octo-robot1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("YES");
+        }
+    }
+}
