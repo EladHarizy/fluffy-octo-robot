d6ce3568a3944f14216a1ad75ad35cabba88f6a9
Abraham Murciano <abrahammurciano@gmail.com>
2020-03-05 21:47:05 +0200

    Renamed project (removed 1) & introduced octo-bot

---
+++ fluffy-octo-robot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace fluffy_octo_robot
+{
+    class fluffy_octo_robot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the fluffy octo-robot");
+            Console.WriteLine("Beep Beep Bop Boop");
+        }
+    }
+}
