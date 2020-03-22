8f341dc12889a53db1de967b94346d843a98c609
Abraham Murciano <abrahammurciano@gmail.com>
2020-03-22 02:31:14 +0100

    Finished menu structure

--- Main.cs
+++ Main.cs
@@ -1,15 +1,38 @@
 ﻿namespace FluffyOctoRobot {
-	class FluffyOctoRobot {
+	class main {
 		static void Main(string[] args) {
 			Menu menu = new Menu(
-				new GuestRequestOption,
-				new ShowOccupancyOption,
-				new ShowStatsOption,
-				new ExitOption
+				"Welcome to the Fluffy Octo-Robot!\nHere you can manage the rent of your villa.\nWhat would you like to do?",
+				new Menu.Option(
+					"Add a new visit.",
+					() => {
+						// Elad, this is for you. This is what should execute when the user picks option a
+						return true; // True because we want the menu to repeat once the option does its task
+					}
+				),
+				new Menu.Option(
+					"Show the yearly occupancy dates.",
+					() => {
+						// Elad, this is for you. This is what should execute when the user picks option b
+						return true;
+					}
+				),
+				new Menu.Option(
+					"Show occupancy statistics.",
+					() => {
+						// Elad, this is for you. This is what should execute when the user picks option c
+						return true;
+					}
+				),
+				new Menu.Option(
+					"Exit",
+					() => {
+						return false;
+					}
+				)
 			);
-			while (true) {
 
-			}
+			menu.Init();
 		}
 	}
 }
--- Menu.cs
+++ Menu.cs
@@ -3,25 +3,77 @@ using System.Collections.Generic;
 
 namespace FluffyOctoRobot {
 	class Menu {
-		List<Option> options = new List<Option>();
+		// The question or prompt that will be displayed to the user
+		private string prompt;
+
+		// The list of options that will be displayed to the user
+		private List<Option> options = new List<Option>();
 
 		// Constructor that takes any number of options
-		public Menu(params Option[] options) {
+		public Menu(string prompt, params Option[] options) {
+			this.prompt = prompt;
 			foreach (Option option in options) {
 				this.options.Add(option);
 			}
 		}
 
-		// Function that prints all the options
-		void Print() {
+		// Function that prints all the options and labels them with a number (starting from 1)
+		private void Print() {
+			Console.Clear();
+			Console.WriteLine(prompt + '\n');
+
+			int i = 0;
 			foreach (Option option in options) {
-				// TODO
+				Console.WriteLine("\t{1} - {0}", options[i].Label, ++i);
+			}
+			Console.WriteLine();
+		}
+
+		// Function that prompts for an option between 1 and options.Count and returns the number of the chosen option minus 1
+		private int Read() {
+			while (true) {
+				Console.Write("Choose an option [1-{0}]: ", options.Count);
+				int choice;
+				try {
+					choice = Int32.Parse(Console.ReadLine());
+					if (choice < 1 || choice > options.Count) {
+						throw new System.IO.InvalidDataException();
+					}
+					return choice - 1; // Minus 1 because options are displayed from 1
+				} catch (Exception e) when(e is FormatException || e is System.IO.InvalidDataException) {
+					Console.WriteLine("Error: Input must be an integer between 1 and {0}.", options.Count);
+				}
 			}
 		}
 
-		// Function that first calls print, then inputs an option, and calls the Execute method of the corresponding option (using polymorphism)
-		public Option Prompt() {
-			// TODO
+		// Function that calls print, then inputs an option, and executes the function of the corresponding option
+		public void Init() {
+			bool repeat;
+			do {
+				Print();
+				int index = Read();
+				repeat = options[index].Execute();
+			} while (repeat);
+		}
+
+		public class Option {
+			private string label;
+			public string Label {
+				get => label;
+			}
+
+			// A function that will be called when the user chooses this option
+			// It should take no parameters and return a bool indicating if the menu should be displayed again
+			private Func<bool> function;
+			public Func<bool> Execute {
+				get => function;
+			}
+
+			// Constructor
+			public Option(string label, Func<bool> function) {
+				this.label = label;
+				this.function = function;
+			}
 		}
 	}
 }
