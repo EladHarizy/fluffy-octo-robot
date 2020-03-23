33c804f3d5617582bf1633f73de2ff9ccc76ce56
Abraham Murciano <abrahammurciano@gmail.com>
2020-03-23 01:18:16 +0100

    Made Option function able to take any parameters.

--- Menu.cs
+++ Menu.cs
@@ -2,29 +2,32 @@ using System;
 using System.Collections.Generic;
 
 namespace FluffyOctoRobot {
-	class Menu {
+	partial class Menu {
 		// The question or prompt that will be displayed to the user
-		private string prompt;
+		private string intro;
 
 		// The list of options that will be displayed to the user
 		private List<Option> options = new List<Option>();
 
 		// Constructor that takes any number of options
-		public Menu(string prompt, params Option[] options) {
-			this.prompt = prompt;
+		public Menu(string intro, params Option[] options) {
+			this.intro = intro;
 			foreach (Option option in options) {
 				this.options.Add(option);
 			}
 		}
 
-		// Function that prints all the options and labels them with a number (starting from 1)
-		private void Print() {
+		private void Intro() {
 			Console.Clear();
-			Console.WriteLine(prompt + '\n');
+			Console.WriteLine(intro);
+		}
 
+		// Function that prints all the options and labels them with a number (starting from 1)
+		private void PrintOptions() {
+			Console.WriteLine();
 			int i = 0;
 			foreach (Option option in options) {
-				Console.WriteLine("\t{1} - {0}", option.Label, ++i);
+				Console.WriteLine("{1} - {0}", option.Label, ++i);
 			}
 			Console.WriteLine();
 		}
@@ -48,32 +51,13 @@ namespace FluffyOctoRobot {
 
 		// Function that calls print, then inputs an option, and executes the function of the corresponding option
 		public void Init() {
+			Intro();
 			bool repeat;
 			do {
-				Print();
+				PrintOptions();
 				int index = Read();
 				repeat = options[index].Execute();
 			} while (repeat);
-		}
-
-		public class Option {
-			private string label;
-			public string Label {
-				get => label;
-			}
-
-			// A function that will be called when the user chooses this option
-			// It should take no parameters and return a bool indicating if the menu should be displayed again
-			private Func<bool> function;
-			public Func<bool> Execute {
-				get => function;
-			}
-
-			// Constructor
-			public Option(string label, Func<bool> function) {
-				this.label = label;
-				this.function = function;
-			}
 		}
 	}
 }
