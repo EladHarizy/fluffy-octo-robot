596da6d881f40c922a05d083b4515ca89e060695
Elad Harizy <elad.harizy@gmail.com>
2020-03-27 01:06:20 +0200

    did more work on try catch Booking

--- Calendar.Booking.cs
+++ Calendar.Booking.cs
@@ -3,7 +3,6 @@ namespace FluffyOctoRobot {
 	partial class Calendar {
 		class Booking {
 			// duration of visit
-			//	private int duration;
 			public int Duration {
 				get;
 				private set;
@@ -14,8 +13,18 @@ namespace FluffyOctoRobot {
 
 			//constructor that takes in initial date and duration
 			public Booking(DateTime dt, int duration) {
-				Duration = duration;
-				this.dateTime = dt;
+				Console.Write("Enter Start Date: ");
+				DateTime choice;
+				try {
+					choice = DateTime.Parse(Console.ReadLine());
+					if (choice < DateTime.Now) {
+						throw new System.IO.InvalidDataException();
+					}
+					Duration = duration;
+					this.dateTime = dt;
+				} catch {
+
+				}
 			}
 
 			// returns the end date
