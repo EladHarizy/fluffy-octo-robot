31a6ec04473d1f2ac837f0ae88b7fc8ab953ead9
Abraham Murciano <abrahammurciano@gmail.com>
2020-04-01 15:50:54 +0200

    Added function to check if two bookings overlap

--- Calendar.Booking.cs
+++ Calendar.Booking.cs
@@ -14,15 +14,22 @@ namespace FluffyOctoRobot {
 				private set;
 			}
 
+			// Returns the end date
+			public DateTime End {
+				get => Start.AddDays(Duration);
+			}
+
 			// Constructor that takes in initial date and duration
 			public Booking(DateTime start, int duration) {
 				Duration = duration;
 				Start = start;
 			}
 
-			// Returns the end date
-			public DateTime End() {
-				return Start.AddDays(Duration);
+			// Function that given two bookings will return true if they overlap
+			public bool Overlaps(Booking booking) {
+				// Two bookings will overlap iff one starts inside the other
+				return (this.Start >= booking.Start && this.Start < booking.End)
+					|| (booking.Start >= this.Start && booking.Start < this.End);
 			}
 		}
 	}
