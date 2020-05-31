3093410441fe3a652afa4f36e8ebcc710f93d5ef
Abraham Murciano <abraham@CT-5555.localdomain>
2020-05-31 23:25:00 +0300

    Created TooManyDigitsException for when an id is passed with too many digits

---
+++ src/exceptions/TooManyDigitsException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exceptions {
+	public class TooManyDigitsException : Exception {
+		public int N {
+			get;
+			private set;
+		}
+
+		public int Digits {
+			get;
+			private set;
+		}
+
+		private string message;
+		public override string Message {
+			get => message;
+		}
+
+		public TooManyDigitsException(int n, int digits) {
+			N = n;
+			Digits = digits;
+		}
+
+		public TooManyDigitsException(int n, int digits, string message) : this(n, digits) {
+			this.message = message;
+		}
+	}
+}
