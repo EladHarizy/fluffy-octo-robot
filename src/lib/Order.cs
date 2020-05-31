964561630493cd4bd761bb224d3fc7faad7e76c1
Abraham Murciano <abraham@CT-5555.localdomain>
2020-06-01 02:10:21 +0300

    fixed line spacing and property access modifiers

--- src/lib/Order.cs
+++ src/lib/Order.cs
@@ -1,43 +1,53 @@
 using System;
-using System.ComponentModel;
 using System.Text;
 
 namespace lib {
-    public class Order {
+	public class Order {
+		private static IDGenerator id_generator = new IDGenerator(8);
+		public ID ID {
+			get;
+			private set;
+		}
 
-        private static IDGenerator id_generator = new IDGenerator(8);
-        public ID ID {
-            get;
-            private set;
-        }
+		private HostingUnit hosting_unit;
+		public ID HostingUnitKey {
+			get => hosting_unit.ID;
+		}
 
-        private HostingUnit hosting_unit;
-        public ID HostingUnitKey {
-            get => hosting_unit.ID;
-        }
-        private GuestRequest guest_request;
-        public ID GuestRequestKey {
-            get => guest_request.ID;
-        }
+		private GuestRequest guest_request;
+		public ID GuestRequestKey {
+			get => guest_request.ID;
+		}
 
-        private Status status;
+		public Status status {
+			get;
+			set;
+		}
 
-        DateTime CreateDate;
-        DateTime OrderDate;
+		public DateTime CreationDate {
+			get;
+			private set;
+		}
 
-        Order(HostingUnit hosting_unit, GuestRequest guest_request) {
-            this.hosting_unit = hosting_unit;
-            this.guest_request = guest_request;
-        }
+		// Email delivery date to customer (We'll have to come up with a more descriptive name for this variable)
+		public DateTime OrderDate {
+			get;
+			private set;
+		}
 
-        public override string ToString() {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Order Details:");
-            sb.AppendLine("Hosting Unit Key: " + HostingUnitKey);
-            sb.AppendLine("Guest Request Key: " + GuestRequestKey);
-            sb.AppendLine("Order Key: " + ID.ToString());
-            sb.AppendLine("Status: " + status);
-            return sb.ToString();
-        }
-    }
+		Order(HostingUnit hosting_unit, GuestRequest guest_request) {
+			this.hosting_unit = hosting_unit;
+			this.guest_request = guest_request;
+		}
+
+		public override string ToString() {
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("Order Details:");
+			sb.AppendLine("Hosting Unit Key: " + HostingUnitKey);
+			sb.AppendLine("Guest Request Key: " + GuestRequestKey);
+			sb.AppendLine("Order Key: " + ID.ToString());
+			sb.AppendLine("Status: " + status);
+			return sb.ToString();
+		}
+	}
 }
