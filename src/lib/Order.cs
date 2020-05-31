31b9c39bf0eb10bec29c83d706cf3df433f13c30
Elad Harizy <elad.harizy@gmail.com>
2020-06-01 00:50:26 +0300

    more updates in orders

--- src/lib/Order.cs
+++ src/lib/Order.cs
@@ -4,17 +4,20 @@ using System.Text;
 
 namespace lib {
     public class Order {
+
+        private static IDGenerator id_generator = new IDGenerator(8);
+        public ID ID {
+            get;
+            private set;
+        }
+
         private HostingUnit hosting_unit;
-        public string HostingUnitKey {
+        public ID HostingUnitKey {
             get => hosting_unit.ID;
         }
         private GuestRequest guest_request;
-        public string GuestRequestKey {
+        public ID GuestRequestKey {
             get => guest_request.ID;
-        }
-        private int order_id;
-        public string OrderID {
-            get => order_id.ToString("D8");
         }
 
         private Status status;
@@ -22,16 +25,19 @@ namespace lib {
         DateTime CreateDate;
         DateTime OrderDate;
 
+        Order(HostingUnit hosting_unit, GuestRequest guest_request) {
+            this.hosting_unit = hosting_unit;
+            this.guest_request = guest_request;
+        }
+
         public override string ToString() {
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("Order Details:");
             sb.AppendLine("Hosting Unit Key: " + HostingUnitKey);
             sb.AppendLine("Guest Request Key: " + GuestRequestKey);
-            sb.AppendLine("Order Key: " + OrderID);
+            sb.AppendLine("Order Key: " + ID.ToString());
             sb.AppendLine("Status: " + status);
             return sb.ToString();
-
         }
-
     }
 }
