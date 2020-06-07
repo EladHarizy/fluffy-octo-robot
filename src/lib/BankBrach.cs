26dda1f13e91f30e6fe68cf56aee7f59f2b813fb
Elad Harizy <elad.harizy@gmail.com>
2020-06-07 23:08:51 +0300

    modified BrankBranch cls, properties & tostring()

--- src/lib/BankBrach.cs
+++ src/lib/BankBrach.cs
@@ -4,41 +4,73 @@ using System.Text;
 namespace lib {
 	public class BankBranch {
 
-		BankBranch(int bank_id, string bank_name, int branch_id, string branch_city) {
-			this.bank_id = bank_id;
-			BankName = bank_name;
-			this.branch_id = branch_id;
-			BranchCity = branch_city;
-		}
-		private int bank_id;
-		public string BankID {
-			get => bank_id.ToString("D4");
-		}
 		public string BankName {
 			get;
 			private set;
 		}
-		private int branch_id;
-		public string BranchID {
-			get => branch_id.ToString("D4");
-		}
+
 		public string BranchAddress {
 			get;
 			private set;
 		}
-		public string BranchCity {
+
+		public City BranchCity {
 			get;
 			private set;
 		}
 
-		public override string ToString() {
+		public ID BankID {
+			get;
+			private set;
+		}
+
+		public ID BranchID {
+			get;
+			private set;
+		}
+
+		BankBranch(ID bank_id, string bank_name, ID branch_id, City branch_city) {
+			this.BankID = bank_id;
+			BankName = bank_name;
+			this.BranchID = branch_id;
+			BranchCity = branch_city;
+		}
+
+		public string ToString(int tabs) {
 			StringBuilder sb = new StringBuilder();
-			sb.AppendLine("Bank Details:");
-			sb.AppendLine("Bank Name - " + BankName);
-			sb.AppendLine("Bank Number - " + BankID.ToString());
-			sb.AppendLine("Branch Number - " + BranchID.ToString());
-			sb.AppendLine("Branch Address - " + BranchAddress);
-			sb.AppendLine("Branch City - " + BranchCity);
+			sb.Append('\t', tabs);
+			sb.Append("Bank Details:");
+			sb.Append("\n");
+
+			sb.Append('\t', tabs);
+			sb.Append("-------------");
+			sb.Append("\n");
+
+			sb.Append('\t', tabs);
+			sb.Append("Bank Name:\t\t");
+			sb.Append(BankName);
+			sb.Append("\n");
+
+			sb.Append('\t', tabs);
+			sb.Append("Bank Number:\t");
+			sb.Append(BankID);
+			sb.Append("\n");
+
+			sb.Append('\t', tabs);
+			sb.Append("Branch Number:\t");
+			sb.Append(BranchID);
+			sb.Append("\n");
+
+			sb.Append('\t', tabs);
+			sb.Append("Branch Address:\t");
+			sb.Append(BranchAddress);
+			sb.Append("\n");
+
+			sb.Append('\t', tabs);
+			sb.Append("Branch City:\t");
+			sb.Append(BranchCity);
+			sb.Append("\n");
+
 			return sb.ToString();
 		}
 	}
