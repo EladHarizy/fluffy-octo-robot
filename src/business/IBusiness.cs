f6b8bb55899226d6417b3e6fa8163908615ed40e
Abraham Murciano <abrahammurciano@gmail.com>
2020-06-19 17:03:08 +0300

    added groupings to ibusiness

--- src/business/IBusiness.cs
+++ src/business/IBusiness.cs
@@ -1,39 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Lib.Entities;
 
 namespace business {
 	public interface IBusiness {
-		// Add a customer request
 		void AddGuestRequest(GuestRequest guest_request);
 
-		// Customer request update (status change)
 		void UpdateGuestRequest(GuestRequest guest_request);
 
-		// Add a hosting unit
 		void AddUnit(Unit unit);
 
-		// Delete a hosting unit
 		void DeleteUnit(Unit unit);
 
-		// Hosting unit update
 		void UpdateUnit(Unit unit1);
 
-		// Add an order
 		void AddOrder(Order order);
 
-		// Order Update (Status Change)
 		void UpdateOrder(Order order);
 
-		// Get a list of all accommodation units
 		ICollection<Unit> Units();
 
-		// Receive a list of all customer requests
 		ICollection<GuestRequest> GuestRequests();
 
-		// Receive a list of all orders
 		ICollection<Order> Orders();
 
-		// Receives a list of all existing bank branches in Israel
 		ICollection<BankBranch> BankBranches();
+
+		// Groupings
+		IEnumerable<IGrouping<City, GuestRequest>> GuestRequestsByCity();
+
+		IEnumerable<IGrouping<int, GuestRequest>> GuestRequestsByGuestCount();
+
+		IEnumerable<IGrouping<int, Host>> HostsByUnitCount();
+
+		IEnumerable<IGrouping<City, Unit>> UnitsByCity();
 	}
 }
