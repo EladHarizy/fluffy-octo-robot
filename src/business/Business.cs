f99c3ba7f2d222173c931f4d16cbe0a522393e70
Elad Harizy <elad.harizy@gmail.com>
2020-06-21 17:51:30 +0300

    IBusiness changes

--- src/business/Business.cs
+++ src/business/Business.cs
@@ -1,5 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Lib.Entities;
+
 namespace business {
 	public class Business : IBusiness {
+		public ICollection<GuestRequest> AllCustomerRequirements(Predicate<GuestRequest> condition) {
+			throw new NotImplementedException();
+		}
 
+		public ICollection<Order> AllOrders(params Date[] dates) {
+			throw new NotImplementedException();
+		}
+
+		public ICollection<Unit> AvailableUnits(Date date, int duration) {
+			throw new NotImplementedException();
+		}
+
+		public int NumberOfDays(Date date1, Date date2) {
+			throw new NotImplementedException();
+		}
 	}
 }
--- src/business/IBusiness.cs
+++ src/business/IBusiness.cs
@@ -1,5 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using Lib.Entities;
 
 namespace business {
-	public interface IBusiness {}
+	public interface IBusiness {
+		ICollection<Unit> AvailableUnits(Date date, int duration);
+
+		int NumberOfDays(Date date1, Date date2);
+
+		ICollection<Order> AllOrders(params Date[] dates);
+
+		ICollection<GuestRequest> AllCustomerRequirements(Predicate<GuestRequest> condition);
+	}
 }
