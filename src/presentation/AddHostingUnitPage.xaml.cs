005dd4e7d29e7d2716ab697383ec0b4c42ab137e
Abraham Murciano <abrahammurciano@gmail.com>
2020-07-07 17:32:33 +0300

    changed MaterialDesign namespace to md

--- src/presentation/AddHostingUnitPage.xaml.cs
+++ src/presentation/AddHostingUnitPage.xaml.cs
@@ -1,14 +1,22 @@
+using System.Windows;
 using System.Windows.Controls;
 using business;
 
 namespace presentation {
 	public partial class AddHostingUnitPage : Page {
-		private IBusiness business;
-		private Frame frame;
+		private IBusiness Business { get; }
+		private Frame Frame { get; }
 
 		public AddHostingUnitPage(IBusiness business, Frame frame) {
-			this.business = business;
-			this.frame = frame;
+			Business = business;
+			Frame = frame;
+		}
+		private void AddHostingUnit(object sender, RoutedEventArgs e) {
+
+		}
+
+		private void Cancel(object sender, RoutedEventArgs e) {
+			Frame.GoBack();
 		}
 	}
 }
